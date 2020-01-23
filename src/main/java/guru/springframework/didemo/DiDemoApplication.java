3a9539e111a490fe2128a4d01d70487fdc719a01
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-01-23 16:27:28 +0100

    Different types of DI add

--- src/main/java/guru/springframework/didemo/DiDemoApplication.java
+++ src/main/java/guru/springframework/didemo/DiDemoApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.didemo;
 
+import guru.springframework.didemo.controllers.ConstructorInjectedController;
 import guru.springframework.didemo.controllers.MyController;
+import guru.springframework.didemo.controllers.PropertyInjectedController;
+import guru.springframework.didemo.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,6 +17,10 @@ public class DiDemoApplication {
         MyController controller = (MyController) applicationContext.getBean("myController");
 
         controller.hello();
+
+        System.out.println(applicationContext.getBean(PropertyInjectedController.class).sayHello());
+        System.out.println(applicationContext.getBean(SetterInjectedController.class).sayHello());
+        System.out.println(applicationContext.getBean(ConstructorInjectedController.class).sayHello());
     }
 
 }
---
+++ src/main/java/guru/springframework/didemo/services/impl/GreetingServiceImpl.java
@@ -0,0 +1,15 @@
+package guru.springframework.didemo.services.impl;
+
+import guru.springframework.didemo.services.GreetingService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingServiceImpl implements GreetingService {
+
+    private static final String HELLO = "Hello Guru";
+
+    @Override
+    public String sayHello() {
+        return HELLO;
+    }
+}
