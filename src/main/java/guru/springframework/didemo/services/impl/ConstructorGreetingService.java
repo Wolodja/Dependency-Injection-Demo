cd4701af9a363d6daa906006965f4690b7486e94
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-01-23 16:44:25 +0100

    Qualifier annotation add

---
+++ src/main/java/guru/springframework/didemo/services/impl/ConstructorGreetingService.java
@@ -0,0 +1,16 @@
+package guru.springframework.didemo.services.impl;
+
+import guru.springframework.didemo.services.GreetingService;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class ConstructorGreetingService implements GreetingService {
+
+    private static final String I_M_FROM_CONSTRUCTOR_GREETING_SERVICE = "I'm from ConstructorGreetingService";
+
+    @Override
+    public String sayHello() {
+        return I_M_FROM_CONSTRUCTOR_GREETING_SERVICE;
+    }
+}
---
+++ src/main/java/guru/springframework/didemo/services/impl/SetterGreetingService.java
@@ -0,0 +1,15 @@
+package guru.springframework.didemo.services.impl;
+
+import guru.springframework.didemo.services.GreetingService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SetterGreetingService implements GreetingService {
+
+    private static final String I_AM_FROM_SETTER_GREETING_SERVICE = "I am from SetterGreetingService";
+
+    @Override
+    public String sayHello() {
+    return I_AM_FROM_SETTER_GREETING_SERVICE;
+    }
+}
