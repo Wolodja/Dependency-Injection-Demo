6213878b8def8edaac7c5730b0210134eb3997d6
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-01-30 09:22:46 +0100

    Property Sources add

--- src/main/java/guru/springframework/didemo/DiDemoApplication.java
+++ src/main/java/guru/springframework/didemo/DiDemoApplication.java
@@ -1,9 +1,7 @@
 package guru.springframework.didemo;
 
-import guru.springframework.didemo.controllers.ConstructorInjectedController;
 import guru.springframework.didemo.controllers.MyController;
-import guru.springframework.didemo.controllers.PropertyInjectedController;
-import guru.springframework.didemo.controllers.SetterInjectedController;
+import guru.springframework.didemo.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -18,11 +16,10 @@ public class DiDemoApplication {
 
         MyController controller = (MyController) applicationContext.getBean("myController");
 
-        System.out.println(controller.hello());
+        FakeDataSource fakeDataSource = applicationContext.getBean(FakeDataSource.class);
 
-        System.out.println(applicationContext.getBean(PropertyInjectedController.class).sayHello());
-        System.out.println(applicationContext.getBean(SetterInjectedController.class).sayHello());
-        System.out.println(applicationContext.getBean(ConstructorInjectedController.class).sayHello());
+        System.out.println(fakeDataSource);
+
     }
 
 }
